380f367e50ff0d75eaf9b6c2ca510730f2b3662c
Pedro Trindade <pedromarcio04@gmail.com>
2026-06-28 21:55:41 -0300

    feat: add TCP and UDP port parsing

--- capture.go
+++ capture.go
@@ -12,12 +12,31 @@ func StartCapture(handle *pcap.Handle) {
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
 	for packet := range packetSource.Packets() {
-		// Tenta extrair camada de rede
+		// Extrai camada IP
 		ipLayer := packet.Layer(layers.LayerTypeIPv4)
 		if ipLayer != nil {
 			ip, _ := ipLayer.(*layers.IPv4)
-			fmt.Printf("IP: %s -> %s | Protocol: %s\n", 
-				ip.SrcIP, ip.DstIP, ip.Protocol)
+
+			// Tenta extrair TCP
+			tcpLayer := packet.Layer(layers.LayerTypeTCP)
+			if tcpLayer != nil {
+				tcp, _ := tcpLayer.(*layers.TCP)
+				fmt.Printf("[TCP] %s:%d -> %s:%d\n",
+					ip.SrcIP, tcp.SrcPort, ip.DstIP, tcp.DstPort)
+				continue
+			}
+
+			// Tenta extrair UDP
+			udpLayer := packet.Layer(layers.LayerTypeUDP)
+			if udpLayer != nil {
+				udp, _ := udpLayer.(*layers.UDP)
+				fmt.Printf("[UDP] %s:%d -> %s:%d\n",
+					ip.SrcIP, udp.SrcPort, ip.DstIP, udp.DstPort)
+				continue
+			}
+
+			// Se não for TCP/UDP, só mostra o protocolo
+			fmt.Printf("[%s] %s -> %s\n", ip.Protocol, ip.SrcIP, ip.DstIP)
 		}
 	}
 }
